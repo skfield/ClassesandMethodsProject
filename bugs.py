e5dafe0f4bf2192d5862e02e90452dc38e779960
skfield <32680211+skfield@users.noreply.github.com>
2020-12-06 11:55:35 -0500

    Update bugs.py

--- bugs.py
+++ bugs.py
@@ -1,21 +1,24 @@
 import pygame
 import mybugs
 
-#colors gotten from rgb website
-
-green = (0, 255, 0)
+white = ( 255, 255, 255)
+green = (0,255,0)
 blue = (0, 0, 255)
-brown = (125, 100, 100)
-yellow = (255, 255, 0)
-
+brown = ( 125, 100, 100)
 
 pygame.init()
-size = [1400,1050]
+size=[1400,1050]
 screen=pygame.display.set_mode(size)
 
-class Garden:
-  def show()
-    pygame.draw.rect(screen,brown,[0, 700, 1400, 325])
-    pygame.draw.rect(screen,green,[0, 600, 1400, 125])
+class Garden
+def show():
+    pygame.draw.rect(screen,brown,[0, 700, 1400, 350])
+    pygame.draw.rect(screen,green,[0, 600, 1400, 150])
     pygame.draw.rect(screen,blue,[0, 0, 1400, 600])
-    pygame.draw.ellipse(screen,yellow,[900, 100, 100, 50])
+    pygame.draw.ellipse(screen,white,[120, 60, 180, 80])
+    pygame.draw.ellipse(screen,white,[700, 80, 150, 60])
+done=False
+clock=pygame.time.Clock()
+
+buglist = []
+temp = []
